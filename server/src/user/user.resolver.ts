e0595f98664bca46f8a0906eb2d158ce31eb0eff
陳宜庭 <studenttim@chenyitingdeMacBook-Pro-2.local>
2022-01-15 22:32:43 +0800

    feat(user): add user resolver and current user decorator

--- server/src/user/user.resolver.ts
+++ server/src/user/user.resolver.ts
@@ -1,9 +1,14 @@
+import { UseGuards } from '@nestjs/common';
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { Role } from '../auth/dto/role.enum';
+import { Roles } from '../auth/decorator/role.decorator';
+import { JwtAuthGraphQLGuard } from '../auth/jwt-auth-graphql.guard';
 import { CreateUserInput } from './dto/create-user.input';
 import { FindUsersInput } from './dto/get-user-input';
 import { UserEntity } from './entities/user.entity';
 import { User, UserResult } from './model/user.model';
 import { UserService } from './user.service';
+import { CurrentUser } from './decorator/current-user.decorator';
 
 @Resolver(() => User)
 export class UserResolver {
@@ -14,6 +19,8 @@ export class UserResolver {
     return this.userService.create(createUserInput);
   }
 
+  @UseGuards(JwtAuthGraphQLGuard)
+  @Roles(Role.Admin, Role.User) /** @todo remove user */
   @Query(() => UserResult)
   async users(@Args() input: FindUsersInput): Promise<UserResult> {
     const users = await this.userService.findAllUsers(input);
@@ -21,8 +28,17 @@ export class UserResolver {
     return { users };
   }
 
+  @UseGuards(JwtAuthGraphQLGuard)
   @Query(() => User)
+  @Roles(Role.Admin, Role.Public, Role.User)
   user(@Args('id') id: string): Promise<UserEntity> {
     return this.userService.findById(id);
+  }
+
+  @UseGuards(JwtAuthGraphQLGuard)
+  @Query(() => User)
+  @Roles(Role.Admin, Role.Public, Role.User)
+  me(@CurrentUser() currentUser: User): Promise<UserEntity> {
+    return this.userService.findById(currentUser.id);
   }
 }
