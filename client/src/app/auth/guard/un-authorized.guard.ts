12778637d47135a1535105bc0f3e47f3393d3b68
陳宜庭 <studenttim@chenyitingdeMacBook-Pro-2.local>
2022-01-17 23:58:41 +0800

    feat(client/auth): append auth services

---
+++ client/src/app/auth/guard/un-authorized.guard.ts
@@ -0,0 +1,25 @@
+import { Injectable } from "@angular/core";
+import { CanActivate, Router, UrlTree } from "@angular/router";
+import { Observable } from "rxjs";
+import { Authenticator } from "../authenticator.service";
+
+@Injectable()
+export class UnAuthorizedGuard implements CanActivate {
+  constructor(
+    private auth: Authenticator,
+    private router: Router,
+  ) {}
+
+  canActivate(): Observable<boolean|UrlTree>|Promise<boolean|UrlTree>|boolean|UrlTree {
+    if (
+      this.auth.authorized
+      && this.router.routerState.snapshot.url.match(/\/login/)
+    ) {
+      this.router.navigate(['']);
+
+      return false;
+    }
+
+    return !this.auth.authorized;
+  }
+}
