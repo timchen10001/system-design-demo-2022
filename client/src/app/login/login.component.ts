5bf08b419aac67bce132846649e0d5b575c75e97
陳宜庭 <studenttim@chenyitingdeMacBook-Pro-2.local>
2022-01-18 00:04:32 +0800

    feat(client/login): applied authorized and un-authorized state handling

--- client/src/app/login/login.component.ts
+++ client/src/app/login/login.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Authenticator } from '../auth/authenticator.service';
+import { Tokens } from '../auth/typings/token.interface';
+import { LoginUserInput } from '../user/typings/login-user.input';
+import { UserService } from '../user/user.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +12,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  loginForm = this.fb.group({
+    email: ['', Validators.required],
+    password: ['', Validators.required],
+  })
 
-  constructor() { }
+  logined = this.auth.authorized;
+
+  constructor(
+    private fb: FormBuilder,
+    private userService: UserService,
+    private auth: Authenticator,
+    private router: Router,
+  ) { }
 
   ngOnInit(): void {
+    this.auth.logined.asObservable().subscribe((logined) => {
+      this.logined = !!logined;
+
+      if (this.logined) {
+        this.router.navigate(['/']);
+      }
+    });
   }
 
+  onSubmit() {
+    const loginArgs: LoginUserInput = this.loginForm.value;
+
+    const loginUser$ = this.userService.getLoginUserObservable(loginArgs);
+
+    loginUser$.subscribe(
+      (loginUserResponse) => {
+        this.auth.updateTokens(loginUserResponse as Tokens);
+      },
+    );
+  }
 }
